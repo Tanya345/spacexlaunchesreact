388a2a64e1fd4c2b71541a9ef2d9f80e4cfdabdb
Tanya345 <tb4407@gmail.com>
2022-05-07 10:57:19 +0530

    Done final updates

--- src/App.js
+++ src/App.js
@@ -5,59 +5,25 @@ import React, { useState, useEffect, createContext } from 'react';
 export const AppContext = createContext([]);
 
 function App() {
-  const [firstRender, setFirstRender] = useState(true)
   const [data, setData] = useState([]);
   const [spaceDetails, setSpaceDetails] = useState({
     launch_success: '',
     land_success: '',
     launch_year: ''
   })
-  let [api, setApi] = useState('https://api.spaceXdata.com/v3/launches?limit=100');
-  useEffect(() => {
-    async function fetchData() {
-      if (firstRender) {
-        setFirstRender(false)
-        let result = await fetch(api);
-        setData(await result.json());
-      }
-      else {
-        if (launch_success) {
-          if (!api.includes('launch_success')) {
-            setApi(prevState => prevState.concat(`&launch_success=${launch_success}`));
-          }
-        }
-        if (land_success) {
-          if (!api.includes('land_success')) {
-            setApi(prevState => prevState.concat(`&land_success=${land_success}`));
-          }
-          
-          // console.log(api);
-
-        }
-        if (launch_year) {
-          if (!api.includes('launch_year')) {
-            setApi(prevState => prevState.concat(`&launch_year=${launch_year}`));
-          }
-
-        }
-      }
-    }
-    fetchData()
-  }, [spaceDetails])
-
-  useEffect(() => {
-    async function fetchData() {
-      if (!firstRender) {
-        let result = await fetch(api);
-        setData(await result.json());
-      }
-    }
-    fetchData()
-  }, [api])
-
-
   let { launch_success, land_success, launch_year } = spaceDetails
-  
+
+  let api = `https://api.spaceXdata.com/v3/launches?limit=100&launch_success=${launch_success}&land_success=${land_success}&launch_year=${launch_year}`;
+
+  useEffect(() => {
+    async function fetchData() {
+      let result = await fetch(api);
+      setData(await result.json());
+    }
+
+    fetchData()
+  }, [spaceDetails, api])
+
   return (
     <AppContext.Provider value={{ data, spaceDetails, setSpaceDetails }}>
       <div>
